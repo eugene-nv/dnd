ede2ae559b4145b17b61b5bfb96480adcc72cb12
Наумкин Евгений <94268804+eugene-nv@users.noreply.github.com>
2023-02-28 10:17:43 +0500

    create templates

--- dungeons_and_dragons/character/urls.py
+++ dungeons_and_dragons/character/urls.py
@@ -4,5 +4,5 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='home'),
-    path('character_list/<slug:char>/', character_list),
+    path('character_list/', character_list, name='character_list'),
 ]
--- dungeons_and_dragons/character/views.py
+++ dungeons_and_dragons/character/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 
+from character.models import CharacterCreate
+
+menu = ['Главная страница', 'Список персонажей', 'Создать персонажа', 'Войти']
+
 
 def index(request):
-    return HttpResponse('Главная страница сайта')
+    return render(request, 'character/index.html', {'menu': menu, 'title': 'Главная страница'})
 
 
-def character_list(request, char):
-    return HttpResponse(f'<h1>Список созданных персонажей</h1><p>{char}</p>')
+def character_list(request):
+    character = CharacterCreate.objects.all()
+    return render(request, 'character/character_list.html', {'menu': menu, 'title': 'Список персонажей', 'character': character})
 
 
 def pageNotFound(request, exception):
