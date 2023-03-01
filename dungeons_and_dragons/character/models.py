8326605e6986139944f25fa41c75a86f40a03f35
Наумкин Евгений <94268804+eugene-nv@users.noreply.github.com>
2023-03-01 14:26:05 +0500

    model correction

--- dungeons_and_dragons/character/models.py
+++ dungeons_and_dragons/character/models.py
@@ -3,82 +3,82 @@ from django.urls import reverse
 
 
 class CharacterCreate(models.Model):
-    class CharacterCard(models.Model):
-        DWARF = 'Дварф'
-        ELF = 'Эльф'
-        HALFLING = 'Полурослик'
-        HUMAN = 'Человек'
-        DRAGONBORN = 'Драконорожденный'
-        GNOME = 'Гном'
-        HALF_ELF = 'Полуэльф'
-        HALF_ORC = 'Полуорк'
-        TIEFLING = 'Тифлинг'
+    DWARF = 'Дварф'
+    ELF = 'Эльф'
+    HALFLING = 'Полурослик'
+    HUMAN = 'Человек'
+    DRAGONBORN = 'Драконорожденный'
+    GNOME = 'Гном'
+    HALF_ELF = 'Полуэльф'
+    HALF_ORC = 'Полуорк'
+    TIEFLING = 'Тифлинг'
 
-        RACE = [
-            (DWARF, 'Дварф'),
-            (ELF, 'Эльф'),
-            (HALFLING, 'Полурослик'),
-            (HUMAN, 'Человек'),
-            (DRAGONBORN, 'Драконорожденный'),
-            (GNOME, 'Гном'),
-            (HALF_ELF, 'Полуэльф'),
-            (HALF_ORC, 'Полуорк'),
-            (TIEFLING, 'Тифлинг'),
+    RACE = [
+        (DWARF, 'Дварф'),
+        (ELF, 'Эльф'),
+        (HALFLING, 'Полурослик'),
+        (HUMAN, 'Человек'),
+        (DRAGONBORN, 'Драконорожденный'),
+        (GNOME, 'Гном'),
+        (HALF_ELF, 'Полуэльф'),
+        (HALF_ORC, 'Полуорк'),
+        (TIEFLING, 'Тифлинг'),
+
+    ]
+
+    LG = 'Законно - добрый'
+    NG = 'Нейтрально - добрый'
+    CG = 'Хаотично - добрый'
+    LN = 'Законно - нейтральный'
+    N = 'Нейтральный'
+    CN = 'Хаотично - нейтральный'
+    LE = 'Законно - злой'
+    NE = 'Нейтрально - злой'
+    CE = 'Хаотично - злой'
+
+    IDEOLOGY = [
+        (LG, 'Законно - добрый'),
+        (NG, 'Нейтрально - добрый'),
+        (CG, 'Хаотично - добрый'),
+        (LN, 'Законно - нейтральный'),
+        (N, 'Нейтральный'),
+        (CN, 'Хаотично - нейтральный'),
+        (LE, 'Законно - злой'),
+        (NE, 'Нейтрально - злой'),
+        (CE, 'Хаотично - злой'),
 
         ]
 
-        LG = 'Законно - добрый'
-        NG = 'Нейтрально - добрый'
-        CG = 'Хаотично - добрый'
-        LN = 'Законно - нейтральный'
-        N = 'Нейтральный'
-        CN = 'Хаотично - нейтральный'
-        LE = 'Законно - злой'
-        NE = 'Нейтрально - злой'
-        CE = 'Хаотично - злой'
+    MALE = 'Мужской'
+    FEMALE = 'Женский'
 
-        IDEOLOGY = [
-            (LG, 'Законно - добрый'),
-            (NG, 'Нейтрально - добрый'),
-            (CG, 'Хаотично - добрый'),
-            (LN, 'Законно - нейтральный'),
-            (N, 'Нейтральный'),
-            (CN, 'Хаотично - нейтральный'),
-            (LE, 'Законно - злой'),
-            (NE, 'Нейтрально - злой'),
-            (CE, 'Хаотично - злой'),
-
+    GENDER = [
+        (MALE, 'Мужской'),
+        (FEMALE, 'Женский'),
         ]
 
-        MALE = 'Мужской'
-        FEMALE = 'Женский'
+    name = models.CharField('Имя', max_length=20)
+    race = models.CharField('Раса', max_length=20, choices=RACE)
+    ideology = models.CharField('Мировоззрение', max_length=50, choices=IDEOLOGY)
+    description = models.TextField('Описание персонажа', max_length=1000)
+    gender = models.CharField('Пол', max_length=20, choices=GENDER)
+    portrait = models.ImageField('Портрет', upload_to='portraits/')
+    strenght = models.IntegerField('Сила')
+    dexterity = models.IntegerField('Ловкость')
+    constitution = models.IntegerField('Телосложение')
+    intellect = models.IntegerField('Интеллект')
+    wizdom = models.IntegerField('Мудрость')
+    charisma = models.IntegerField('Харизма')
 
-        GENDER = [
-            (MALE, 'Мужской'),
-            (FEMALE, 'Женский'),
-        ]
+    DoesNotExist = models.Manager
 
-        name = models.CharField('Имя', max_length=20)
-        race = models.CharField('Раса', max_length=20, choices=RACE)
-        ideology = models.CharField('Мировоззрение', max_length=50, choices=IDEOLOGY)
-        description = models.TextField('Описание персонажа', max_length=1000)
-        gender = models.CharField('Пол', max_length=20, choices=GENDER)
-        portrait = models.ImageField('Портрет', upload_to='portraits/')
-        strenght = models.IntegerField('Сила')
-        dexterity = models.IntegerField('Ловкость')
-        constitution = models.IntegerField('Телосложение')
-        intellect = models.IntegerField('Интеллект')
-        wizdom = models.IntegerField('Мудрость')
-        charisma = models.IntegerField('Харизма')
+    def __str__(self):
+        return self.name
 
-        DoesNotExist = models.Manager
+    class Meta:
+        verbose_name = 'Карточка персонажа'
+        verbose_name_plural = 'Карточки персонажей'
 
-        def __str__(self):
-            return self.name
 
-        def get_absolute_url(self):
-            return reverse('character', kwargs={'character_id': self.pk})
-
-        class Meta:
-            verbose_name = 'Карточка персонажа'
-            verbose_name_plural = 'Карточки персонажей'
+def get_absolute_url(self):
+    return reverse('character', kwargs={'character_id': self.pk})
