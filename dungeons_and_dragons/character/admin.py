a4fe6ba36ff88a43e27586f8e08696191e4b67a5
Наумкин Евгений <94268804+eugene-nv@users.noreply.github.com>
2023-03-03 09:17:56 +0500

    customize admin

--- dungeons_and_dragons/character/admin.py
+++ dungeons_and_dragons/character/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(CharacterCreate)
+
+class CharacterCreateAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'race', 'ideology', 'gender')
+    list_display_links = ('id', 'name')
+    search_fields = ('name','race', 'ideology')
+
+
+admin.site.register(CharacterCreate, CharacterCreateAdmin)
--- dungeons_and_dragons/character/apps.py
+++ dungeons_and_dragons/character/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class CharacterConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'character'
+    verbose_name = 'Карточки персонажей'
