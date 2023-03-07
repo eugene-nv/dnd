60650b489fb79bf7d8a8b57ea33924d1bb9ec145
Наумкин Евгений <94268804+eugene-nv@users.noreply.github.com>
2023-03-07 13:39:22 +0500

    little changes

--- dungeons_and_dragons/character/forms.py
+++ dungeons_and_dragons/character/forms.py
@@ -3,14 +3,6 @@ from .models import *
 
 
 class AddCharacterForm(forms.ModelForm):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['race'].empty_label = 'Выберете расу'
-        self.fields['klass'].empty_label = 'Выберете класс'
-        self.fields['ideology'].empty_label = 'Выберете мировоззрение'
-        self.fields['gender'].empty_label = 'Выберете пол'
-
-
     class Meta:
         model = CharacterCreate
         fields = ['name', 'race', 'klass', 'ideology', 'description', 'gender', 'portrait',
